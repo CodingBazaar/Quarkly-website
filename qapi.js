cbd161a628861843b8352241927b971cca697d49
Sagar-Joshi <37098603+CodingBazaar@users.noreply.github.com>
2020-12-14 13:12:26 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd7168c00ea36001e3bb6ed",
+				"5fd7168c00ea36001e3bb6ef"
+			]
+		},
+		"5fd7168c00ea36001e3bb6ed": {
+			"id": "5fd7168c00ea36001e3bb6ed",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd7168c00ea36001e3bb6ef": {
+			"id": "5fd7168c00ea36001e3bb6ef",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
